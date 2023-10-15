1116f0763b49a4c0eb62a706fb85bae4e7997255
Cavallo-Davide <146706550+Cavallo-Davide@users.noreply.github.com>
2023-10-15 22:48:10 +0200

    Create MezzoDiTrasporto.java

---
+++ cavallo_davide/es_5_pag_52/MezzoDiTrasporto.java
@@ -0,0 +1,11 @@
+public class MezzoDiTrasporto {
+    String tipo;
+
+    public MezzoDiTrasporto(String tipo) {
+        tipo = tipo;
+    }
+
+    public void muovi() {
+        System.out.println("Il mezzo di trasporto si sta muovendo.");
+    }
+}
