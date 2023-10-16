2843600d8b583b409ab317eee8c9108e7dfc05c9
Compostella-Alberto <146766598+Compostella-Alberto@users.noreply.github.com>
2023-10-16 06:31:12 +0200

    Create mucca.java

---
+++ compostella_alberto/es1_pag52/mucca.java
@@ -0,0 +1,14 @@
+public class mucca extends mammifero {
+    private String colore;
+    public mucca(){
+        super();
+        colore = null;
+    }
+    public void SetColore(String colore){
+        this.colore = colore;
+    }
+
+    public String getcolore(){
+        return colore;
+    }
+}
