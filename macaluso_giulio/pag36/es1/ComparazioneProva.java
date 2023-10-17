aee44b52d52ba02005adc59a2d4d71e232656b16
Macaluso-Giulio <146706582+Macaluso-Giulio@users.noreply.github.com>
2023-10-17 22:09:35 +0200

    Create ComparazioneProva.java

---
+++ macaluso_giulio/pag36/es1/ComparazioneProva.java
@@ -0,0 +1,9 @@
+public class ComparazioneProva {
+    public static void main(String[] args) {
+        Comparazione comp = new Comparazione("stringa1");
+        comp.CalcolaStringa("stringa");
+
+        System.out.println(comp);
+
+    }
+}
