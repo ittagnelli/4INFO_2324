eac239e7241a18f8e316148f1d92c1add2175e65
Macaluso-Giulio <146706582+Macaluso-Giulio@users.noreply.github.com>
2023-10-24 20:32:06 +0200

    Create Mezzo.java

---
+++ macaluso_giulio/pag52/es9/Mezzo.java
@@ -0,0 +1,11 @@
+public class Mezzo {
+    private int ruote;
+
+    public Mezzo(int r) {
+        ruote = r;
+    }
+
+    public void stampa() {
+        System.out.println("ruote: " + ruote);
+    }
+}
