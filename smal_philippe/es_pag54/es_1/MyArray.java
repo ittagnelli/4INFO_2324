7653255623780c33378c2a0e5a0eb62abd0e1664
Smal-Philippe <146706932+Smal-Philippe@users.noreply.github.com>
2023-10-24 18:56:41 +0200

    Create MyArray.java

---
+++ smal_philippe/es_pag54/es_1/MyArray.java
@@ -0,0 +1,36 @@
+public class MyArray {
+    private int[] mioArray;
+    static final int TANTI = 7;
+
+    public MyArray(int[] array_enter) {
+        mioArray = array_enter.clone();
+    }
+
+    public boolean cerca(int numero) {
+        for(int i = 0; i < TANTI; i++) {
+            if(mioArray[i] == numero) return true;
+        }
+        return false;
+    }
+
+    public void single() {
+        for (int i = 0; i < TANTI; i++) {
+            for (int j = 0; j < TANTI; j++) {
+                if ((mioArray[i] == mioArray[j]) && (j != i)) {
+                    mioArray[i] = 0;
+                }
+            }
+        }
+    }
+
+    public String toString() {
+        String msg = "";
+        for(int i = 0; i < TANTI; i++) {
+            if(mioArray[i] != 0) msg += mioArray[i] + ", ";
+        }
+
+        return msg;
+    }
+
+
+}
