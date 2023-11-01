c5120b293103de43ef2801cc8b90fdeade433cb4
Cavallo-Davide <146706550+Cavallo-Davide@users.noreply.github.com>
2023-11-02 00:01:09 +0100

    Create Main.java

---
+++ cavallo_davide/es_1_pag55/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Auto automobile = new Auto("gg 333 tt", "ford", "kuga", 2022, "Grigia");
+        System.out.println(automobile);
+        Camion camion = new Camion("gg 333 tt", "fiat", "ibrido", 1970, 20000);
+        System.out.println(camion);
+
+    }
+}
