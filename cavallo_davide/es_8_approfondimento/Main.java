03d381163216a29ad9c9f1343ae04bb761783fae
Cavallo-Davide <146706550+Cavallo-Davide@users.noreply.github.com>
2023-11-05 23:09:34 +0100

    Create Main.java

---
+++ cavallo_davide/es_8_approfondimento/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Archivio archivio = new Archivio();
+
+        archivio.aggiungiBrano("Titolo Brano 1", "CD 1", "Artista 1", "Genere 1");
+
+        archivio.aggiungiBrano("Titolo Brano 2", "CD 2", "Artista 2", "Genere 2");
+    }
+}
