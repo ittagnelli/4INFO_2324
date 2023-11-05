b4442f8326e695a329eff64afb04cc3b54f54aec
Cavallo-Davide <146706550+Cavallo-Davide@users.noreply.github.com>
2023-11-05 23:08:09 +0100

    Create Artista.java

---
+++ cavallo_davide/es_8_approfondimento/Artista.java
@@ -0,0 +1,11 @@
+public class Artista {
+    private String nome;
+
+    public Artista(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
