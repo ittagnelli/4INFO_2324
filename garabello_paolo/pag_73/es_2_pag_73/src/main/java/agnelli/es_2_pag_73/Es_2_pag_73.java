4583a85cc6238fea93b9c7767876c9d5b2b8b503
Paolo <paolo.garabello06@gmail.com>
2023-11-08 11:01:18 +0100

    esercizio 2 pag 73

---
+++ garabello_paolo/pag_73/es_2_pag_73/src/main/java/agnelli/es_2_pag_73/Es_2_pag_73.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package agnelli.es_2_pag_73;
+
+/**
+ *
+ * @author paolo
+ */
+public class Es_2_pag_73 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
