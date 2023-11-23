2a6f3098b13abb34dae107accf123b0e770ef4d8
Sebastiano <racca.sebastiano.7805@gmail.com>
2023-11-23 08:48:58 +0100

    Aggiunta esercitazione `pg_82`

---
+++ racca_sebastiano/unita_2/pg_82/src/main/java/agnelli/listbox/ListBox.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package racca.me.listbox;
+
+/**
+ *
+ * @author seba
+ */
+public class ListBox {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
