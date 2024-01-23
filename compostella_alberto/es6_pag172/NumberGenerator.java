9b4bba317ccd5b4b009aefcf98761cc6af286e7d
Compostella-Alberto <146766598+Compostella-Alberto@users.noreply.github.com>
2024-01-24 00:31:21 +0100

    Create NumberGenerator.java

---
+++ compostella_alberto/es6_pag172/NumberGenerator.java
@@ -0,0 +1,19 @@
+class NumberGenerator {
+    public static ArrayList<Integer> generateNumbers() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Random random = new Random();
+
+
+        while (true) {
+            int num = random.nextInt(21) + 20; 
+            numbers.add(num);
+
+            if (num % 5 == 0) {
+                break;
+            }
+        }
+
+        System.out.println("Numeri generati: " + numbers);
+        return numbers;
+    }
+}
