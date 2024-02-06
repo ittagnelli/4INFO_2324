1f82db44059543039c1558e515db2409371f0a80
Emanuele-Furina <115022990+Emanuele-Furina@users.noreply.github.com>
2024-02-06 21:55:26 +0100

    Create TabellaHashStudenti.java

---
+++ furina_emanuele/esercitazione_studente/MetodoCerca/TabellaHashStudenti.java
@@ -0,0 +1,53 @@
+import java.util.LinkedList;
+
+public class TabellaHashStudenti {
+    private static final int DIMENSIONE =  10;
+    private LinkedList<Studente>[] studenti;
+
+    public TabellaHashStudenti() {
+        studenti = new LinkedList[DIMENSIONE];
+        for (int i =  0; i < DIMENSIONE; i++) {
+            studenti[i] = new LinkedList<>();
+        }
+    }
+
+    private int funzioneHash(int matricola) {
+        return matricola % DIMENSIONE;
+    }
+
+    public void inserisci(Studente studente) {
+        int indice = funzioneHash(studente.getMatricola());
+        studenti[indice].add(studente);
+    }
+
+    public boolean contiene(Studente studente) {
+        int indice = funzioneHash(studente.getMatricola());
+        return studenti[indice].contains(studente);
+    }
+
+    public Studente cerca(int id) {
+        int indice = funzioneHash(id);
+        for (Studente studente : studenti[indice]) {
+            if (studente.getMatricola() == id) {
+                return studente;
+            }
+        }
+        return null;
+    }
+    
+    public void rimuovi(Studente studente) {
+        int indice = funzioneHash(studente.getMatricola());
+        studenti[indice].remove(studente);
+    }
+
+    public void stampa() {
+        for (int i =  0; i < DIMENSIONE; i++) {
+            System.out.printf("Posizione %d: ", i);
+            for (Studente s : studenti[i]) {
+                System.out.printf("%s-%d, ", s.getNome(), s.getMatricola());
+            }
+            System.out.println();
+        }
+    }
+}
+    
