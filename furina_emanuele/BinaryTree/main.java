ebb7976dc3fc3234f99e5eb6f1c2cca0c67b4722
Emanuele-Furina <115022990+Emanuele-Furina@users.noreply.github.com>
2024-03-04 09:39:33 +0100

    Update main.java

--- furina_emanuele/BinaryTree/main.java
+++ furina_emanuele/BinaryTree/main.java
@@ -7,5 +7,15 @@ public class main{
         bT.addNode(50);
         bT.addNode(11);
         bT.BFSVisit();
+        int value = 50;
+        boolean trovato = bT.BtsSearch(bT.root, value);
+
+        if (trovato) {
+            System.out.println("si");
+        }
+        else
+        {
+            System.out.println("No");
+        }
     }
 } 
