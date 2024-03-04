0a899384b95bbea2da5480231fa2b05eaab07384
Emanuele-Furina <115022990+Emanuele-Furina@users.noreply.github.com>
2024-03-04 09:19:34 +0100

    Create main.java

---
+++ furina_emanuele/BinaryTree/main.java
@@ -0,0 +1,11 @@
+public class main{
+    public static void main(String[] args) {
+        BinaryTree bT= new BinaryTree();
+        bT.addNode(20);
+        bT.addNode(9);
+        bT.addNode(7);
+        bT.addNode(50);
+        bT.addNode(11);
+        bT.BFSVisit();
+    }
+} 
